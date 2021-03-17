5c305e334331c81148077c50ef57c2830ac72d98
geoyee <geoyee@yeah.net>
2021-03-17 19:51:34 +0800

    update ppcd/losses/losses.py

--- ppcd/losses/losses.py
+++ ppcd/losses/losses.py
@@ -4,65 +4,6 @@ import paddle.nn.functional as F
 
 
 class BCELoss(nn.Layer):
-    """
-    This operator combines the sigmoid layer and the :ref:`api_nn_loss_BCELoss` layer.
-    Also, we can see it as the combine of ``sigmoid_cross_entropy_with_logits``
-    layer and some reduce operations.
-    This measures the element-wise probability error in classification tasks
-    in which each class is independent.
-    This can be thought of as predicting labels for a data-point, where labels
-    are not mutually exclusive. For example, a news article can be about
-    politics, technology or sports at the same time or none of these.
-    First this operator calculate loss function as follows:
-    .. math::
-           Out = -Labels * \\log(\\sigma(Logit)) - (1 - Labels) * \\log(1 - \\sigma(Logit))
-    We know that :math:`\\sigma(Logit) = \\frac{1}{1 + \\e^{-Logit}}`. By substituting this we get:
-    .. math::
-           Out = Logit - Logit * Labels + \\log(1 + \\e^{-Logit})
-    For stability and to prevent overflow of :math:`\\e^{-Logit}` when Logit < 0,
-    we reformulate the loss as follows:
-    .. math::
-           Out = \\max(Logit, 0) - Logit * Labels + \\log(1 + \\e^{-\|Logit\|})
-    Then, if ``weight`` or ``pos_weight`` is not None, this operator multiply the
-    weight tensor on the loss `Out`. The ``weight`` tensor will attach different
-    weight on every items in the batch. The ``pos_weight`` will attach different
-    weight on the positive label of each class.
-    Finally, this operator applies reduce operation on the loss.
-    If :attr:`reduction` set to ``'none'``, the operator will return the original loss `Out`.
-    If :attr:`reduction` set to ``'mean'``, the reduced mean loss is :math:`Out = MEAN(Out)`.
-    If :attr:`reduction` set to ``'sum'``, the reduced sum loss is :math:`Out = SUM(Out)`.
-    Note that the target labels ``label`` should be numbers between 0 and 1.
-    Args:
-        weight (Tensor | str, optional): A manual rescaling weight given to the loss of each
-            batch element. If given, it has to be a 1D Tensor whose size is `[N, ]`,
-            The data type is float32, float64. If type is str, it should equal to 'dynamic'.
-            It will compute weight dynamically in every step.
-            Default is ``'None'``.
-        pos_weight (float|str, optional): A weight of positive examples. If type is str,
-            it should equal to 'dynamic'. It will compute weight dynamically in every step.
-            Default is ``'None'``.
-        ignore_index (int64, optional): Specifies a target value that is ignored
-            and does not contribute to the input gradient. Default ``255``.
-        edge_label (bool, optional): Whether to use edge label. Default: False
-    Shapes:
-        logit (Tensor): The input predications tensor. 2-D tensor with shape: [N, *],
-            N is batch_size, `*` means number of additional dimensions. The ``logit``
-            is usually the output of Linear layer. Available dtype is float32, float64.
-        label (Tensor): The target labels tensor. 2-D tensor with the same shape as
-            ``logit``. The target labels which values should be numbers between 0 and 1.
-            Available dtype is float32, float64.
-    Returns:
-        A callable object of BCEWithLogitsLoss.
-    Examples:
-        .. code-block:: python
-            import paddle
-            paddle.disable_static()
-            logit = paddle.to_tensor([5.0, 1.0, 3.0], dtype="float32")
-            label = paddle.to_tensor([1.0, 0.0, 1.0], dtype="float32")
-            bce_logit_loss = paddle.nn.BCEWithLogitsLoss()
-            output = bce_logit_loss(logit, label)
-            print(output.numpy())  # [0.45618808]
-    """
     def __init__(self,
                  weight=None,
                  pos_weight=None,
