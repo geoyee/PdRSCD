da5bbf87a367ee47d9d10f5bf481ed9df6bf33ff
geoyee <geoyee@yeah.net>
2021-07-27 16:25:34 +0800

    add shp2array and tif2shp

--- ppcd/tools/__init__.py
+++ ppcd/tools/__init__.py
@@ -1,3 +1,3 @@
 from .tailor_base import random_out, slide_out, split_eval
 from .splicing import splicing_list
-from .geo_process import open_tif, tif2array, get_geoinfo, save_tif
+from .geo_process import *
--- ppcd/tools/geo_process.py
+++ ppcd/tools/geo_process.py
@@ -1,4 +1,8 @@
+import os
+import re
+import fnmatch
 import numpy as np
+from PIL import Image
 try:
     try:
         from osgeo import gdal
@@ -71,3 +75,42 @@ def save_tif(img, geoinfo, save_path):
         del dataset  # 删除与tif的连接
     else:
         raise ImportError('can\'t import gdal!')
+
+
+def shp2array(shp_file, tif_file):
+    from osgeo import osr
+
+    geoimg = gdal.Open(tif_file)
+    trans = geoimg.GetGeoTransform()
+    cols = geoimg.RasterXSize
+    rows = geoimg.RasterYSize
+    mem = gdal.GetDriverByName('MEM')
+    mid_ds = mem.Create('', cols, rows, 1, gdal.GDT_Byte)
+    mid_ds.SetGeoTransform(trans)
+    mid_ds.SetMetadataItem('AREA_OR_POINT', 'Point')
+    mid_ds.GetRasterBand(1).WriteArray(np.ones((rows, cols), dtype=np.bool))
+    srs = osr.SpatialReference()
+    srs.SetWellKnownGeogCS(geoimg.GetProjection())
+    mid_ds.SetProjection(srs.ExportToWkt())
+    mask_ds = gdal.Warp('', mid_ds, format='MEM', cutlineDSName=shp_file)
+    return mask_ds.ReadAsArray()
+
+
+def tif2shp(tif_path):
+    from osgeo import ogr, osr
+
+    inraster = gdal.Open(tif_path)
+    inband = inraster.GetRasterBand(1)
+    prj = osr.SpatialReference()  
+    prj.ImportFromWkt(inraster.GetProjection())
+    outshp = tif_path[:-4] + ".shp"  # 矢量输出文件名
+    drv = ogr.GetDriverByName("ESRI Shapefile")
+    if os.path.exists(outshp):  # 若文件已经存在，则删除它继续重新做一遍
+        drv.DeleteDataSource(outshp)
+    Polygon = drv.CreateDataSource(outshp)  # 创建一个目标文件
+    Poly_layer = Polygon.CreateLayer(tif_path[:-4], srs=prj, geom_type=ogr.wkbMultiPolygon)
+    newField = ogr.FieldDefn('value', ogr.OFTReal)  # 用来存储原始栅格的pixel value
+    Poly_layer.CreateField(newField)
+    gdal.FPolygonize(inband, None, Poly_layer, 0)  # 核心函数，执行的就是栅格转矢量操作
+    Polygon.SyncToDisk() 
+    Polygon = None
